e0c651935b4450ca95df6861b014a5750086f967
Dmitrii <33090339+DmitriiKhoreshko@users.noreply.github.com>
2025-02-18 22:44:44 +0500

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -20,9 +20,9 @@ if uploaded_file is not None:
     # Now do something with the image! For example, let's display it:
     res=mask()
 
-    prediction=res.get_mask("./best.pt",opencv_image,0)
+    maskarray=res.get_mask("./best.pt",opencv_image,0)
 
-    ready_image,mask1=res.get_mask_n_masked_im(prediction["mask"],np.copy(opencv_image),0.5,100)
+    ready_image,mask1=res.get_mask_n_masked_im(maskarray,np.copy(opencv_image),0.5,100)
     im_pil = Image.fromarray(ready_image)
     st.image(im_pil, caption="Обработанное изображение", use_container_width=True) 
     st.image(mask1, caption="Обработанное изображение", use_container_width=True)
