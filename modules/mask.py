87dbda45047d7d742ffde5c3291d82ea62f35986
Dmitrii <mr.khoreshko@mail.ru>
2025-02-18 15:48:26 +0500

    tgyjdhjg

---
+++ modules/mask.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import torch
+from PIL import Image  
+
+# image="./test_images/test.jpg"
+
+# model_path = "runs/segment/XL_Original_300_epochs/weights/best.pt"
+
+class mask:
+    def get_mask_n_image(self, model_path , image, conf):
+        
+        model = YOLO(model_path)
+        
+        result = model.predict(image, show_boxes=False, imgsz=832, iou=1, conf=conf, max_det=1000)
+        
+        print("Тут я уже")
+        
+        return {"mask":result[0].masks.xy, "image": np.copy(image)}
+
+    def mask_image(self, masks, image, transparensy,trash):  
+            
+            transparency = transparensy  # Уровень прозрачности (значение от 0 до 1)  
+            alpha_value = int(255 * transparency)
+            
+            b_mask = np.zeros((image.shape[0], image.shape[1], 4), np.uint8) 
+
+            for mask in masks:
+                try:
+                    contour = mask
+
+                    if mask.shape[0]>=trash:
+                        
+                        contour = contour.astype(np.int32)
+                        
+                        contour = contour.reshape(-1, 1, 2)
+                        
+                        b_mask = cv2.drawContours(b_mask, [contour], -1, (0, 255, 0, 0), cv2.FILLED)
+                    else:
+                        continue
+                except:
+                    pass
+            alpha_channel = b_mask[:, :, 3] 
+            alpha_channel[alpha_channel > 0] = alpha_value 
+            alpha_channel[alpha_channel == 0] = 0
+ 
+            b_mask_rgb = b_mask[:, :, :3]
+            isolated = cv2.addWeighted(image, 1, b_mask_rgb, transparency, 0)
+
+            return isolated  
+        
+    def show_image(self,image,key):
+        cv2.imshow("image",image)
+        cv2.waitKey(key)
+            
+# predict=mask()
+
+# mask1=predict.get_mask(model_path,image,0)
+
+# predict.show(predict.masking(mask1["mask"],mask1["image"], 0.2,100),0)
