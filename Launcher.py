251407481b66d5a6377b355632ab11fc02d4ce8e
Dmitrii <mr.khoreshko@mail.ru>
2025-02-19 13:04:11 +0500

    Fix

--- Launcher.py
+++ Launcher.py
@@ -7,90 +7,23 @@ from modules.mask  import *
 
 # Заголовок приложения  
 st.title("Загрузка и просмотр изображений")  
-
 # Установка конфигурации загрузки изображений  
-uploaded_files = st.file_uploader("Выберите изображения", type=["jpg", "jpeg", "png"], accept_multiple_files=True)  
+uploaded_file = st.file_uploader("Выберите изображения", type=["jpg", "jpeg", "png"], accept_multiple_files=False)  
+    
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
 
-# Переменные для хранения выбранного изображения и обработанных изображений в сессии  
-if 'selected_image' not in st.session_state:  
-    st.session_state.selected_image = None  
+    st.image(opencv_image, caption="Выбранное изображение", use_container_width=True)  
+    # Now do something with the image! For example, let's display it:
+    res=mask()
 
-# Проверка, загружены ли изображения  
-if uploaded_files:  
-    # Отображение загруженных изображений  
-    st.subheader("Список загруженных изображений:")  
+    maskarray=res.get_mask("./best.pt",opencv_image,0)
 
-    # Создаем контейнер для горизонтального списка  
-    cols = st.columns(len(uploaded_files))  
-
-    # Обходим загруженные файлы  
-    for idx, uploaded_file in enumerate(uploaded_files):  
-        img = Image.open(uploaded_file)  
-        with cols[idx]:  
-            # Отображение изображений  
-            st.image(img, caption=uploaded_file.name, use_container_width=True)  
-
-            # Кнопка для выбора изображения  
-            if st.button(f"Обработать {uploaded_file.name}", key=f"process_{uploaded_file.name}"):  
-                st.session_state.selected_image = img  # Сохраняем выбранное изображение   
-                st.session_state.processed_images = []  # Сбрасываем предыдущие обработанные изображения  
-
-    # Отображаем выбранное изображение, если оно есть  
-    if st.session_state.selected_image:  
-        st.subheader("Выбранное изображение:")  
-<<<<<<< HEAD
-        
-        st.image(st.session_state.selected_image, caption="Выбранное изображение", use_container_width=True)  
-        
-        st.subheader("Обработанное изображение:")   
-        
-        model_path = "runs/segment/XL_Original_300_epochs/weights/best.pt"  # Примечание: Замените на корректный путь  
-        model = YOLO(model_path)  # Загружаем модель  
-=======
-        
-        st.image(st.session_state.selected_image, caption="Выбранное изображение", use_container_width=True)  
-        
-        st.subheader("Обработанное изображение:")   
-        
-        model_path = "./best.pt"  # Примечание: Замените на корректный путь  
-        model = YOLO(model_path)  # Загружаем модель  
-
-        # Если обработанные изображения еще не загружены, генерируем их    
-        confidence_levels = [0, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006]  # Уровни уверенности  
-            
-         
->>>>>>> d2d12795a91e3afdf802881758b207e60deb1ac9
-
-        # Если обработанные изображения еще не загружены, генерируем их    
-        
-        
-        confidence_levels = []  # Уровни уверенности  
-        i=0   
-        while i<0.7:
-            i+=0.0001
-            confidence_levels.append(i)
-        # Слайдер для выбора уровня уверенности  
-<<<<<<< HEAD
-        slider_value = st.slider("Precision level:", 1, len(confidence_levels), 1)  
-        
-        res=mask()
-        
-        prediction=res.get_mask_n_image("./best.pt",st.session_state.selected_image,0)
-        
-        ready_image=res.mask_image(prediction["mask"],prediction["image"],0.5,100)
-        
-        st.image(ready_image, caption="Обработанное изображение", use_container_width=True)  # Обратите внимание на метод plot()  
-=======
-        slider_value = st.slider("Выберите уровень уверенности:",   
-                                 1, len(confidence_levels), 1)  
-        
-        result = model.predict(st.session_state.selected_image, show_boxes=False, imgsz=832, iou=1, conf=confidence_levels[slider_value - 1], max_det=1000) 
-        
-        
-        # # Отображаем первое предсказанное изображение  
-        st.write(result[0].masks)
-        #st.image(result[0].save(boxes=False), caption="Обработанное изображение", use_container_width=True)  # Обратите внимание на метод plot() 
->>>>>>> d2d12795a91e3afdf802881758b207e60deb1ac9
-
-else:  
-    st.warning("Пожалуйста, загрузите хотя бы одно изображение.")
+    ready_image,mask1=res.get_mask_n_masked_im(maskarray,np.copy(opencv_image),0.5,100)
+    im_pil = Image.fromarray(ready_image)
+    st.image(im_pil, caption="Обработанное изображение", use_container_width=True) 
+    st.image(mask1, caption="Обработанное изображение", use_container_width=True)
+    cv2.waitKey
--- modules/mask.py
+++ modules/mask.py
@@ -9,17 +9,15 @@ from PIL import Image
 # model_path = "runs/segment/XL_Original_300_epochs/weights/best.pt"
 
 class mask:
-    def get_mask_n_image(self, model_path , image, conf):
-        
+    def get_mask(self, model_path , image, conf):
+        """Получаем на выход маску"""
         model = YOLO(model_path)
         
-        result = model.predict(image, show_boxes=False, imgsz=832, iou=1, conf=conf, max_det=1000)
+        result = model.predict(image, show_boxes=False, imgsz=832, iou=1, conf=conf, max_det=500)
         
-        print("Тут я уже")
-        
-        return {"mask":result[0].masks.xy, "image": np.copy(image)}
+        return result[0].masks.xy
 
-    def mask_image(self, masks, image, transparensy,trash):  
+    def get_mask_n_masked_im(self, masks, image, transparensy,trash):  
             
             transparency = transparensy  # Уровень прозрачности (значение от 0 до 1)  
             alpha_value = int(255 * transparency)
@@ -32,11 +30,14 @@ class mask:
 
                     if mask.shape[0]>=trash:
                         
+                        temp = np.zeros((image.shape[0], image.shape[1], 4), np.uint8) 
+                        
                         contour = contour.astype(np.int32)
                         
                         contour = contour.reshape(-1, 1, 2)
                         
-                        b_mask = cv2.drawContours(b_mask, [contour], -1, (0, 255, 0, 0), cv2.FILLED)
+                        b_mask =cv2.bitwise_or(b_mask, cv2.fillPoly(temp, [contour], (0, 255, 0)))
+                        print(contour)
                     else:
                         continue
                 except:
@@ -47,8 +48,7 @@ class mask:
  
             b_mask_rgb = b_mask[:, :, :3]
             isolated = cv2.addWeighted(image, 1, b_mask_rgb, transparency, 0)
-
-            return isolated  
+            return isolated, b_mask_rgb  
         
     def show_image(self,image,key):
         cv2.imshow("image",image)
