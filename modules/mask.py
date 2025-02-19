f78b782d45741a65e538c56fd7069e9a19525f19
Dmitrii <33090339+DmitriiKhoreshko@users.noreply.github.com>
2025-02-19 12:52:22 +0500

    Update mask.py

--- modules/mask.py
+++ modules/mask.py
@@ -4,12 +4,16 @@ from ultralytics import YOLO
 import torch
 from PIL import Image  
 
+# image="./test_images/test.jpg"
+
+# model_path = "runs/segment/XL_Original_300_epochs/weights/best.pt"
+
 class mask:
     def get_mask(self, model_path , image, conf):
         """Получаем на выход маску"""
         model = YOLO(model_path)
         
-        result = model.predict(image, show_boxes=False, conf=conf)
+        result = model.predict(image, show_boxes=False, imgsz=832, iou=1, conf=conf, max_det=100)
         
         return result[0].masks.xy
 
@@ -20,27 +24,24 @@ class mask:
             
             b_mask = np.zeros((image.shape[0], image.shape[1], 4), np.uint8) 
 
-            # for mask in masks:
-            try:
-                mask=masks[6]
-                contour = mask
+            for mask in masks:
+                try:
+                    contour = mask
 
-                # if mask.shape[0]>=trash:
-                    
-                temp = np.zeros((image.shape[0], image.shape[1], 4), np.uint8) 
-                
-                contour = contour.astype(np.int32)
-                
-                contour = contour.reshape(-1, 1, 2)
-                
-                b_mask =cv2.bitwise_or(b_mask, cv2.fillPoly(temp, [contour], (0, 255, 0)))
-                    
-                # else:
-                #     continue
-            except:
-                pass
-
-            b_mask=temp
+                    if mask.shape[0]>=trash:
+                        
+                        temp = np.zeros((image.shape[0], image.shape[1], 4), np.uint8) 
+                        
+                        contour = contour.astype(np.int32)
+                        
+                        contour = contour.reshape(-1, 1, 2)
+                        
+                        b_mask =cv2.bitwise_or(b_mask, cv2.fillPoly(temp, [contour], (0, 255, 0)))
+                        
+                    else:
+                        continue
+                except:
+                    pass
             alpha_channel = b_mask[:, :, 3] 
             alpha_channel[alpha_channel > 0] = alpha_value 
             alpha_channel[alpha_channel == 0] = 0
@@ -52,3 +53,9 @@ class mask:
     def show_image(self,image,key):
         cv2.imshow("image",image)
         cv2.waitKey(key)
+            
+# predict=mask()
+
+# mask1=predict.get_mask(model_path,image,0)
+
+# predict.show(predict.masking(mask1["mask"],mask1["image"], 0.2,100),0)
