a36a0cca40109eba828da70ba7590500a02c5ca8
Dmitrii <33090339+DmitriiKhoreshko@users.noreply.github.com>
2025-02-19 12:32:03 +0500

    Update mask.py

--- modules/mask.py
+++ modules/mask.py
@@ -13,7 +13,7 @@ class mask:
         """Получаем на выход маску"""
         model = YOLO(model_path)
         
-        result = model.predict(image, show_boxes=False, imgsz=832, iou=1, conf=conf, max_det=100)
+        result = model.predict(image, show_boxes=False, imgsz=832, iou=1, conf=conf, max_det=30)
         
         return result[0].masks.xy
 
