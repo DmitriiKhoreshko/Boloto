463a0e135c048ef060dc8d686c0dcda3119a4639
Dmitrii <33090339+DmitriiKhoreshko@users.noreply.github.com>
2025-02-19 14:47:45 +0500

    Update mask.py

--- modules/mask.py
+++ modules/mask.py
@@ -4,10 +4,6 @@ from ultralytics import YOLO
 import torch
 from PIL import Image  
 
-# image="./test_images/test.jpg"
-
-# model_path = "runs/segment/XL_Original_300_epochs/weights/best.pt"
-
 class mask:
     def get_mask(self, model_path , image, conf):
         """Получаем на выход маску"""
@@ -30,12 +26,11 @@ class mask:
 
                     if mask.shape[0]>=trash:
                         
-                        temp = np.zeros((image.shape[0], image.shape[1], 4), np.uint8) 
+                        contour = contour.astype(np.int32)
                         
-                        contour = contour.astype(np.int32).reshape(-1, 1, 2)
-                        print(contour)
-                        print("Новый")
-                        b_mask =cv2.bitwise_or(b_mask, cv2.drawContours(b_mask, [contour], 2, (0, 255, 0),-1))
+                        contour = contour.reshape(-1, 1, 2)
+                        
+                        b_mask = cv2.fillPoly(b_mask, [contour], (0, 255, 0))  
                     else:
                         continue
                 except:
@@ -47,13 +42,3 @@ class mask:
             b_mask_rgb = b_mask[:, :, :3]
             isolated = cv2.addWeighted(image, 1, b_mask_rgb, transparency, 0)
             return isolated, b_mask_rgb  
-        
-    def show_image(self,image,key):
-        cv2.imshow("image",image)
-        cv2.waitKey(key)
-            
-# predict=mask()
-
-# mask1=predict.get_mask(model_path,image,0)
-
-# predict.show(predict.masking(mask1["mask"],mask1["image"], 0.2,100),0)
